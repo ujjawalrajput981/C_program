ad0da3b3820c06acd55ce07ae1d1d2a28e558b03
ujjawalrajput981 <ujjawalchauhan.701@gmail.com>
2025-09-03 10:02:09 +0530

    ASCII

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character for print its ASCII value : ");
+    scanf("%C",&ch);
+    printf("ASCII value of %c is %d\n",ch,ch);
+    return 0;
+}
